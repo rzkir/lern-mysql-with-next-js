5669247d6fb18294ad7c1b55e28d59fd728089bc
rzkir <rr8027896@gmail.com>
2025-10-30 00:12:18 +0700

    [update] push for sql

--- src/utils/lib/database.ts
+++ src/utils/lib/database.ts
@@ -1,15 +1,13 @@
 import mysql from "mysql2/promise";
 
-// Konfigurasi database MySQL
 const dbConfig = {
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "e-commerce",
-  port: parseInt(process.env.DB_PORT || "3306"),
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT as string),
 };
 
-// Pool connection untuk performa yang lebih baik
 const pool = mysql.createPool({
   ...dbConfig,
   waitForConnections: true,
